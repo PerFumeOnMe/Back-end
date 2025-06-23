0b399fd74d88de23e1f11a8003966599838db71d
bulee5328 <ung5328@hanmail.net>
2025-06-23 20:39:02 +0900

    [Setting] 프로젝트 초기세팅 (#1)

---
+++ src/main/java/PerfumeOnMe/spring/apiPayload/exception/GeneralException.java
@@ -0,0 +1,21 @@
+package PerfumeOnMe.spring.apiPayload.exception;
+
+import PerfumeOnMe.spring.apiPayload.code.BaseErrorCode;
+import PerfumeOnMe.spring.apiPayload.code.ErrorReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private BaseErrorCode code;
+
+    public ErrorReasonDTO getErrorReason() {
+        return this.code.getReason();
+    }
+
+    public ErrorReasonDTO getErrorReasonHttpStatus(){
+        return this.code.getReasonHttpStatus();
+    }
+}
