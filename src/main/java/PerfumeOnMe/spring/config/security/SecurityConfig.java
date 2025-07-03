766e187125f66b6d0cc61334ad7d619b0f1065f6
hcg0127 <kej431003@gmail.com>
2025-07-03 20:07:37 +0900

    [Feature] 문자열 암호화를 위한 PasswordEncoder 빈 등록 (#9)

--- src/main/java/PerfumeOnMe/spring/config/security/SecurityConfig.java
+++ src/main/java/PerfumeOnMe/spring/config/security/SecurityConfig.java
@@ -4,23 +4,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                )
-                .formLogin(config -> config.disable())
-                .httpBasic(config -> config.disable())
-                .csrf(config -> config.disable());
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(auth -> auth
+				.anyRequest().permitAll()
+			)
+			.formLogin(config -> config.disable())
+			.httpBasic(config -> config.disable())
+			.csrf(config -> config.disable());
 
-        return http.build();
-    }
+		return http.build();
+	}
+
+	// 문자열 암호화를 위한 PasswordEncoder 빈 등록
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
 
