7cc04f88476b5d623f33038ee9e96c8a533d034f
hcg0127 <kej431003@gmail.com>
2025-07-03 20:10:29 +0900

    [Feature] 일반적인 에러와 사용자 에러 추가 (#9)

--- src/main/java/PerfumeOnMe/spring/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/PerfumeOnMe/spring/apiPayload/code/status/ErrorStatus.java
@@ -1,40 +1,51 @@
 package PerfumeOnMe.spring.apiPayload.code.status;
 
+import org.springframework.http.HttpStatus;
+
 import PerfumeOnMe.spring.apiPayload.code.BaseErrorCode;
 import PerfumeOnMe.spring.apiPayload.code.ErrorReasonDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorStatus implements BaseErrorCode {
 
-    // 예시,,,
-    ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다.");
+	//일반적인 에러
+	_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
+	_BAD_REQUEST(HttpStatus.BAD_REQUEST, "COMMON400", "잘못된 요청입니다."),
+	_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
+	_FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
-    private final HttpStatus httpStatus;
-    private final String code;
-    private final String message;
+	// 사용자 에러
+	LOGIN_ID_DUPLICATE(HttpStatus.BAD_REQUEST, "MEMBER4001", "이미 사용된 아이디입니다."),
+	PASSWORD_CONFIRM_FAIL(HttpStatus.BAD_REQUEST, "MEMBER4002", "비밀번호 확인을 실패했습니다."),
 
-    @Override
-    public ErrorReasonDTO getReason() {
-        return ErrorReasonDTO.builder()
-                .message(message)
-                .code(code)
-                .isSuccess(false)
-                .build();
-    }
+	// 예시,,,
+	ARTICLE_NOT_FOUND(HttpStatus.NOT_FOUND, "ARTICLE4001", "게시글이 없습니다.");
 
-    @Override
-    public ErrorReasonDTO getReasonHttpStatus() {
-        return ErrorReasonDTO.builder()
-                .message(message)
-                .code(code)
-                .isSuccess(false)
-                .httpStatus(httpStatus)
-                .build()
-                ;
-    }
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String message;
+
+	@Override
+	public ErrorReasonDTO getReason() {
+		return ErrorReasonDTO.builder()
+			.message(message)
+			.code(code)
+			.isSuccess(false)
+			.build();
+	}
+
+	@Override
+	public ErrorReasonDTO getReasonHttpStatus() {
+		return ErrorReasonDTO.builder()
+			.message(message)
+			.code(code)
+			.isSuccess(false)
+			.httpStatus(httpStatus)
+			.build()
+			;
+	}
 }
 
