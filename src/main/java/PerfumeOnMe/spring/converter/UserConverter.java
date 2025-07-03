a4396839c139cda3a76a4f5599ceb35f5e97114d
hcg0127 <kej431003@gmail.com>
2025-07-03 20:13:54 +0900

    [Feature] DTO와 엔티티를 변환하는 메서드 구현 (#9)

--- src/main/java/PerfumeOnMe/spring/converter/UserConverter.java
+++ src/main/java/PerfumeOnMe/spring/converter/UserConverter.java
@@ -1,4 +1,23 @@
 package PerfumeOnMe.spring.converter;
 
+import PerfumeOnMe.spring.domain.User;
+import PerfumeOnMe.spring.web.dto.user.UserResponseDTO;
+
 public class UserConverter {
+
+	// 사용자 회원가입을 위한 엔티티로 변환
+	public static User toSignupUser(String name, String loginId, String password) {
+		return User.builder()
+			.name(name)
+			.loginId(loginId)
+			.password(password)
+			.build();
+	}
+
+	// 사용자를 회원가입 결과 DTO 반환
+	public static UserResponseDTO.SignupResult toSignupResult(User user) {
+		return UserResponseDTO.SignupResult.builder()
+			.userId(user.getId())
+			.build();
+	}
 }
