bec1438511dbfca5e89574cc8bf36579266755f0
hcg0127 <kej431003@gmail.com>
2025-07-03 20:23:22 +0900

    [Fix] lambda를 method reference로 대체 (#9)

--- src/main/java/PerfumeOnMe/spring/config/security/SecurityConfig.java
+++ src/main/java/PerfumeOnMe/spring/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,9 +19,9 @@ public class SecurityConfig {
 			.authorizeHttpRequests(auth -> auth
 				.anyRequest().permitAll()
 			)
-			.formLogin(config -> config.disable())
-			.httpBasic(config -> config.disable())
-			.csrf(config -> config.disable());
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+			.csrf(AbstractHttpConfigurer::disable);
 
 		return http.build();
 	}
