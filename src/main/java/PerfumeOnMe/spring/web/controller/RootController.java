4c0b11e7005e2cfee3788d5a80d8cbf87ac2c3d6
leewonhee-3054 <dldnjsgml3054@naver.com>
2025-07-04 15:55:11 +0900

    [Fix] QueryDSL 설정 충돌 해결 및 의존성 추가 (#8)

--- src/main/java/PerfumeOnMe/spring/web/controller/RootController.java
+++ src/main/java/PerfumeOnMe/spring/web/controller/RootController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
 
-    @GetMapping("/health")
-    public String healthCheck(){
-        return "I'm really healthy";
-    }
+	@GetMapping("/health")
+	public String healthCheck() {
+		return "I'm really healthy";
+	}
 }
