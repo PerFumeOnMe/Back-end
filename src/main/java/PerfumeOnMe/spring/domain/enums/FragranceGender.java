9eb1a5be0205ce4c0d33fb4660ea42b159818461
leewonhee-3054 <dldnjsgml3054@naver.com>
2025-07-06 03:20:50 +0900

    [Fix] 한국어 변환을 위해 FragranceGender 에 koName 필드 추가 (#8)

--- src/main/java/PerfumeOnMe/spring/domain/enums/FragranceGender.java
+++ src/main/java/PerfumeOnMe/spring/domain/enums/FragranceGender.java
@@ -1,5 +1,12 @@
 package PerfumeOnMe.spring.domain.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum FragranceGender {
-    MALE, FEMALE, NEUTRAL
+	MALE("남성용"), FEMALE("여성용"), NEUTRAL("남녀불문");
+
+	private final String koName;     // 한국어 변환
 }
