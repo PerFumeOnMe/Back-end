061f92e73a21bfb3d2374708c19d8a1e99a4fd7b
hcg0127 <kej431003@gmail.com>
2025-07-07 23:22:18 +0900

    [Feature] 로그인 요청 및 응답 DTO 클래스 추가 (#12)

---
+++ src/main/java/PerfumeOnMe/spring/config/security/auth/dto/AuthResponseDTO.java
@@ -0,0 +1,17 @@
+package PerfumeOnMe.spring.config.security.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class AuthResponseDTO {
+
+	@Getter
+	@Builder
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class RefreshToken {
+		private String refreshToken;
+	}
+}
