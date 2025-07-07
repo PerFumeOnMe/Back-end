28a09f1fe77f3722037de9fc8286cb0e86968c80
hcg0127 <kej431003@gmail.com>
2025-07-07 23:20:08 +0900

    [Feature] application.yml에 설정한 token 설정값을 받아오는 클래스 추가 (#12)

---
+++ src/main/java/PerfumeOnMe/spring/config/security/auth/dto/JwtProperties.java
@@ -0,0 +1,24 @@
+package PerfumeOnMe.spring.config.security.auth.dto;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties("jwt.token")
+public class JwtProperties {
+
+	private String secretKey;
+	private Expiration expiration;
+
+	@Getter
+	@Setter
+	public static class Expiration {
+		private Long access;
+		private Long refresh;
+	}
+}
