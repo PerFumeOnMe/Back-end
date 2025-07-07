28de08392d704d74ef6bd641f1a67704519f1bb3
hcg0127 <kej431003@gmail.com>
2025-07-08 00:09:14 +0900

    [Feature] JwtAuthenticationToken과 관련된 인증을 부여하는 JwtAuthenticationProvider 추가 (#12)

---
+++ src/main/java/PerfumeOnMe/spring/config/security/auth/provider/JwtAuthenticationProvider.java
@@ -0,0 +1,34 @@
+package PerfumeOnMe.spring.config.security.auth.provider;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import PerfumeOnMe.spring.config.security.auth.token.JwtAuthenticationToken;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationProvider implements AuthenticationProvider {
+
+	private final JwtTokenProvider jwtTokenProvider;
+	private final UserDetailsService userDetailsService;
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		String token = authentication.getCredentials().toString();
+		String loginId = jwtTokenProvider.getSubject(token);
+
+		UserDetails userDetails = userDetailsService.loadUserByUsername(loginId);
+
+		return new JwtAuthenticationToken(userDetails);
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return JwtAuthenticationToken.class.isAssignableFrom(authentication);
+	}
+}
