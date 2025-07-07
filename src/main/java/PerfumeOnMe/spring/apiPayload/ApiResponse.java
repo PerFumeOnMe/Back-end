c669909e953ae7c5c50842927968f1c983b75f4a
hcg0127 <kej431003@gmail.com>
2025-07-07 23:17:25 +0900

    [Feature] 필터에서 통일된 응답을 반환하기 위한 static 메서드 추가 (#12)

--- src/main/java/PerfumeOnMe/spring/apiPayload/ApiResponse.java
+++ src/main/java/PerfumeOnMe/spring/apiPayload/ApiResponse.java
@@ -1,11 +1,16 @@
 package PerfumeOnMe.spring.apiPayload;
 
+import java.io.IOException;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import PerfumeOnMe.spring.apiPayload.code.BaseCode;
+import PerfumeOnMe.spring.apiPayload.code.status.ErrorStatus;
 import PerfumeOnMe.spring.apiPayload.code.status.SuccessStatus;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -14,6 +19,7 @@ import lombok.Getter;
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 public class ApiResponse<T> {
 
+	private static final ObjectMapper mapper = new ObjectMapper();
 	@JsonProperty("isSuccess")
 	private final Boolean isSuccess;
 	private final String code;
@@ -35,5 +41,19 @@ public class ApiResponse<T> {
 	// 400 CLIENT, 500 SERVER 등 실패한 경우 응답 생성
 	public static <T> ApiResponse<T> onFailure(String code, String message, T data) {
 		return new ApiResponse<>(false, code, message, data);
+	}
+
+	public static void setErrorResponse(HttpServletResponse response,
+		ErrorStatus code, Throwable e) throws IOException {
+
+		// 응답 헤더 작성
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json; charset=UTF-8");
+		response.setStatus(code.getReasonHttpStatus().getHttpStatus().value());
+
+		// 응답 데이터 생성 및 작성
+		ApiResponse<String> res = ApiResponse
+			.onFailure(code.getCode(), code.getMessage(), null); // ???로 찍히는 에러 발생 중
+		response.getWriter().write(mapper.writeValueAsString(res));
 	}
 }
