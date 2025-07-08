3435ee44b0ae0a70880d6e5028db34e8a0186f54
leewonhee-3054 <dldnjsgml3054@naver.com>
2025-07-08 17:15:11 +0900

    [Feature] page, size 관련 커스텀 어노테이션 및 validator 추가(#16)

---
+++ src/main/java/PerfumeOnMe/spring/validation/validator/PageValidator.java
@@ -0,0 +1,13 @@
+package PerfumeOnMe.spring.validation.validator;
+
+import PerfumeOnMe.spring.validation.annotation.ValidPage;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PageValidator implements ConstraintValidator<ValidPage, Integer> {
+
+	@Override
+	public boolean isValid(Integer value, ConstraintValidatorContext context) {
+		return value != null && value >= 0;
+	}
+}
