d7c79aca4002a843a3069d37e38dec8a8f131bae
chanudevelop <chanu.develop@gmail.com>
2025-07-09 13:27:40 +0900

    [Feature] 이미지키워드 엔터티 생성(#22)

--- src/main/java/PerfumeOnMe/spring/domain/ImageKeyword.java
+++ src/main/java/PerfumeOnMe/spring/domain/ImageKeyword.java
@@ -4,7 +4,15 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
 import PerfumeOnMe.spring.domain.base.BaseEntity;
+import PerfumeOnMe.spring.domain.enums.Ambience;
+import PerfumeOnMe.spring.domain.enums.Character;
+import PerfumeOnMe.spring.domain.enums.Gender;
+import PerfumeOnMe.spring.domain.enums.Season;
+import PerfumeOnMe.spring.domain.enums.Style;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -34,5 +42,40 @@ public class ImageKeyword extends BaseEntity {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
 	private User user;
+
+	@Column(length = 50, nullable = false)
+	private String savedName;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Ambience ambience;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Style style;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Gender gender;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Season season;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Character character;
+
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String imageUrl;
+
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String scenario;
+
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String keywordDescription;
+
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String recommendedFragranceJson;
 
 }
