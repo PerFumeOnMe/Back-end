a36d62f757364d41b2b2996d97f0450a458d89d4
bulee5328 <ung5328@hanmail.net>
2025-07-22 01:50:16 +0900

    [Feature] PBTI 결과 조회 API 구현 (#73)

---
+++ src/main/java/PerfumeOnMe/spring/web/dto/Pbti/PbtiRequestDTO.java
@@ -0,0 +1,44 @@
+package PerfumeOnMe.spring.web.dto.Pbti;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+public class PbtiRequestDTO {
+
+	// PBTI 결과 조회 요청 DTO
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	@AllArgsConstructor
+	@ToString
+	public static class PbtiQuestionRequest {
+		@JsonProperty("qOne")
+		private String qOne;
+
+		@JsonProperty("qTwo")
+		private String qTwo;
+
+		@JsonProperty("qThree")
+		private String qThree;
+
+		@JsonProperty("qFour")
+		private String qFour;
+
+		@JsonProperty("qFive")
+		private String qFive;
+
+		@JsonProperty("qSix")
+		private String qSix;
+
+		@JsonProperty("qSeven")
+		private String qSeven;
+
+		@JsonProperty("qEight")
+		private String qEight;
+	}
+}
