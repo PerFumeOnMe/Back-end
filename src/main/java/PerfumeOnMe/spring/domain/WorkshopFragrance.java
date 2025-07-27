a2be8c9f855f5117a6bd933aaaf3e0a2220a5ac0
chanudevelop <chanu.develop@gmail.com>
2025-07-27 18:14:49 +0900

    [feature] 향수공방 결과 생성을 위한 도메인 추가(#94)

---
+++ src/main/java/PerfumeOnMe/spring/domain/WorkshopFragrance.java
@@ -0,0 +1,65 @@
+package PerfumeOnMe.spring.domain;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import PerfumeOnMe.spring.domain.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "workshop_fragrances")
+public class WorkshopFragrance extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 100)
+	private String name; // 향수이름
+
+	@Column(nullable = false, length = 50)
+	private String brand; // 브랜드
+
+	@Column(length = 50)
+	private String mainAccord1; // 메인어코드 1순위
+
+	@Column(length = 50)
+	private String mainAccord2; // 메인어코드 2순위
+
+	@Column(length = 50)
+	private String mainAccord3; // 메인어코드 3순위
+
+	@Column(columnDefinition = "TEXT")
+	private String topNote; // 탑노트
+
+	@Column(columnDefinition = "TEXT")
+	private String middleNote; // 미들노트
+
+	@Column(columnDefinition = "TEXT")
+	private String baseNote; // 베이스노트
+
+	@Column(length = 500)
+	private String imageUrl; // 향수이미지
+
+	@Column(columnDefinition = "TEXT")
+	private String description; // 향수설명
+
+	@Column(nullable = false)
+	private Integer price; // 가격
+}
