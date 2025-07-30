0e8b17e81aed87359175e6f755586637d10774aa
leewonhee-3054 <dldnjsgml3054@naver.com>
2025-07-30 16:52:46 +0900

    [Fix] 데이터시트 관련 변경(#109)

--- src/main/java/PerfumeOnMe/spring/domain/enums/Brand.java
+++ src/main/java/PerfumeOnMe/spring/domain/enums/Brand.java
@@ -10,7 +10,14 @@ public enum Brand {
 	DIPTYQUE("딥티크 (DIPTYQUE)"),
 	JOMALONE("조 말론 (JOMALONE)"),
 	MAISON_MARGIELA("메종 마르지엘라 (MAISON MARGIELA)"),
-	FREDERIC_MALLE("프레데릭 말 (FREDERIC MALLE)");
+	FREDERIC_MALLE("프레데릭 말 (FREDERIC MALLE)"),
+	BYREDO("바이레도 (BYREDO)"),
+	TOM_FORD("톰 포드 (TOM FORD)"),
+	AESOP("이솝 (AESOP)"),
+	YVES_SAINT_LAURENT("입생로랑 (YVES SAINT LAURENT)"),
+	LE_LABO("르 라보 (LE LABO)"),
+	VERSACE("베르사체 (VERSACE)"),
+	MAISON_FRANCIS_KURKDJIAN("메종 프란시스 커정 (MAISON FRANCIS_KURKDJIAN)");
 
 	private final String showBrand;
 }
