67ef3c73ba2aeecc469fb46846ee030eeec3dc73
chanudevelop <chanu.develop@gmail.com>
2025-08-15 01:24:53 +0900

    [hotfix] 오류해결원인찾기

--- src/main/java/PerfumeOnMe/spring/security/SecurityConfig.java
+++ src/main/java/PerfumeOnMe/spring/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -45,7 +44,7 @@ public class SecurityConfig {
 		http
 			// 요청 경로별 인증 확인 설정
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+				// .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 				.requestMatchers(AUTH_WHITELIST).permitAll()
 				.anyRequest().authenticated() // 개발 진행할 때 임시로 풀어두기 -> 나중에 authenticated()로 변경
 			)
