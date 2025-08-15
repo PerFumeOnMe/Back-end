bdb3e375784ef076646d3be01b7edeef4e3c4b90
hcg0127 <kej431003@gmail.com>
2025-08-16 00:54:12 +0900

    [Fix] 로그인 DTO 입력 예시 설정 (#153)

--- src/main/java/PerfumeOnMe/spring/security/auth/dto/AuthRequestDTO.java
+++ src/main/java/PerfumeOnMe/spring/security/auth/dto/AuthRequestDTO.java
@@ -1,5 +1,6 @@
 package PerfumeOnMe.spring.security.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,10 @@ public class AuthRequestDTO {
 	@NoArgsConstructor
 	public static class Login {
 		@NotNull
+		@Schema(description = "사용자가 입력한 아이디", example = "umc123")
 		private String loginId;
 		@NotNull
+		@Schema(description = "사용자가 입력한 비밀번호", example = "asdf1234")
 		private String password;
 	}
 }
